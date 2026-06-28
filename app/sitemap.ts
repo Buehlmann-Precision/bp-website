aafc959f060e343149a2d990ada042a2b41d91c6
Jarno Bühlmann <suppajeator@gmail.com>
2026-06-28 17:27:08 +0200

    Fix sitemap domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://buehlmannprecision.ch";
+  const baseUrl = "https://buehlmann-precision.ch";
 
   return [
     {
